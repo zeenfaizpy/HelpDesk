c546b6a95ce0e4c66a4d695bf50e5893e48b91ca
Mohammad <zeenfaiz@gmail.com>
2015-05-15 15:26:11 +0400

    added urls module

---
+++ users/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import patterns, url
+
+
+# urlpatterns = patterns('',
+#                 url(r'^login/?$',
+#                     LoginView.as_view(),
+#                     name='login'),
+#                 url(r'^logout/?$',
+#                     LogoutView.as_view(),
+#                     name='logout'),
+#                 url(r'^change_password/?$',
+#                     ChangePasswordView.as_view(),
+#                     name='change_password'),
+#                 url(r'^profile/(?P<pk>\d+)/?$',
+#                     ProfileView.as_view(),
+#                     name='user_profile'),
+#                 url(r'^profile/edit/(?P<pk>\d+)/?$',
+#                     EditUserProfile.as_view(),
+#                     name='edit_user_profile'),
+#                 )
