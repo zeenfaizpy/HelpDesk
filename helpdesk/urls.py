c21444612ab514c1f3133137cf08287aa4f637ba
Mohammad <zeenfaiz@gmail.com>
2015-05-15 15:24:48 +0400

    customizing admin header

--- helpdesk/urls.py
+++ helpdesk/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^auth/', include('users.urls')),
+    # url(r'^auth/', include('users.urls')),
 ]
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm 
+from django import forms
 
-# Register your models here.
+from .models import UserProfile
+
+class MyUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = UserProfile
+
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = UserProfile
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        try:
+            UserProfile.objects.get(username=username)
+        except UserProfile.DoesNotExist:
+            return username
+        raise forms.ValidationError(self.error_messages['duplicate_username'])
+
+
+class UserProfileAdmin(UserAdmin):
+    form = MyUserChangeForm
+    add_form = MyUserCreationForm
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('mobile_number',)}),
+    )
+
+admin.site.site_header = 'HelpDesk Administration'
+admin.site.site_title = 'HelpDesk'
+admin.site.index_title = 'HelpDesk Apps'
+
+admin.site.register(UserProfile, UserProfileAdmin)
