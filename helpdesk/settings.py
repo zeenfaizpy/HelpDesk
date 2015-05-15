87b51e73ccd2df769a163c0fbfcd379ae3aaf86e
Mohammad <zeenfaiz@gmail.com>
2015-05-16 01:34:42 +0400

    adding alloweed host

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')!ngqz@q-wcad$o7q(7i3zpra@wa(8z-jxsx4wtcgacqp5d^z6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
