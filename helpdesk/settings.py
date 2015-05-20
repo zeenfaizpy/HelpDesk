d7be037b02374cd7f06ddf34bfabc7162c1cca61
Mohammad <zeenfaiz@gmail.com>
2015-05-20 22:08:39 +0400

    added template dirs

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'helpdesk.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': '',                      
         'USER': '',                      
         'PASSWORD': '',
-        'HOST': '12',                      
+        'HOST': '',                      
         'PORT': '',                 
     }
 }
