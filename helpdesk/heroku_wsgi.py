7be9ac8d2fe7ddf99c4ff2813ac90b948ae0ee1d
Mohammad Faizal <mfaizal@deam.ae>
2015-10-06 15:00:11 +0400

    fixing wsgi files

--- helpdesk/heroku_wsgi.py
+++
@@ -1,9 +0,0 @@
-"""
-Wsgi config for heroku project.
-"""
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "helpdesk.heroku_settings")
-
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-application = Cling(get_wsgi_application())
