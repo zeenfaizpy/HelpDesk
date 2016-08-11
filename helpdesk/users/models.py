140b88729f47c26f11142d3e6f11a573249a2b6d
Mohammad Faizal <zeenfaiz@gmail.com>
2016-08-11 18:33:44 +0400

    user profile model added

--- helpdesk/users/models.py
+++ helpdesk/users/models.py
@@ -1,27 +1,49 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals, absolute_import
+from django.contrib.auth.base_user import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
 
-from django.contrib.auth.models import AbstractUser
-from django.core.validators import RegexValidator
-from django.core.urlresolvers import reverse
-from django.db import models
+from django_countries.fields import CountryField
+
+from .managers import UserManager
 
 
-class User(AbstractUser):
+class UserProfile(AbstractBaseUser, PermissionsMixin):
     """
-    Model to keep registred user data.
+    Custom user model with email as username.
     """
-    name = models.CharField(_('Name of User'), blank=True, max_length=255)
-    mobile_number = models.CharField(max_length=12,
-                                     blank=True,
-                                     validators=[
-                                         RegexValidator(
-                                                regex=r'^\d+$',
-                                                message='Only digits are allowed'
-                                                    )])
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=40, blank=False)
+    last_name = models.CharField(max_length=40, blank=False)
 
-    def __str__(self):
-        return self.username
+    is_staff = models.BooleanField(default=False,
+        help_text='Designates whether the user can log into this admin site.')
+    is_active = models.BooleanField(default=True,
+        help_text='Designates whether this user should be treated as '
+                    'active. Unselect this instead of deleting accounts.')
+    date_joined = models.DateTimeField(default=timezone.now)
 
-    def get_absolute_url(self):
-        return reverse('users:detail', kwargs={'username': self.username})
+    mailing_address = models.TextField(blank=False, default='')
+    current_location = CountryField(null=True)
+    current_city = models.CharField(max_length=120, blank=False, default='')
+    mobile_number = models.CharField(max_length=20, blank=False, default='')
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    last_modified = models.DateTimeField(auto_now=True, null=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        ordering = ['email']
+
+
+    def full_name(self):
+        return u'%s %s' % (self.first_name.strip(), self.last_name.strip())
+
+    def get_short_name(self):
+        return u'%s' % (self.first_name.strip())
+
+    def __unicode__(self):
+        return self.full_name
